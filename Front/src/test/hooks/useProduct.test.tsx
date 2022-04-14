6d6f38bfe2806c942fd447a9617c0a7a47028b3b
Mehdi Mansour <elmehdi.mansour@gmail.com>
2022-04-14 15:11:28 +0200

    adding the test to the remote branch

--- Front/src/test/hooks/useProduct.test.tsx
+++ Front/src/test/hooks/useProduct.test.tsx
@@ -1,13 +1,50 @@
-import { setupServer } from "msw/lib/types/node";
+import { act, renderHook } from "@testing-library/react-hooks";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import useProduct from "../../hooks/useProduct";
 
 
-const server = setupServer();
+const server = setupServer(
+    rest.post("http://localhost:8000/api/cart/61",
+    (req, res, ctx) => {
+        return res(ctx.json({
+            "id": 1,
+            "products": [
+                {
+                    "id": 61,
+                    "name": "Rick Sanchez",
+                    "price": "8",
+                    "quantity": 20,
+                    "image": "rick's image"
+                }
+            ]
+        }));
+    })
+);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test("test add product", async ()=>{
+test("add product to cart", async ()=>{
 
-    expect(true).toBeTruthy();
+    const {result} = renderHook(() => useProduct({
+        "id": 61,
+        "name": "Rick Sanchez",
+        "price": "8",
+        "quantity": 20,
+        "image": "rick's image"
+    }));
+    const {loading, addProduct, setQuantity} = result.current;
+    expect(loading).toEqual(false);
+
+    await act(async () => {
+        await setQuantity(5);
+        await addProduct();
+    });
+
+    const {message, loading: afterLoading} = result.current;
+
+    expect(message).toEqual("Enregistré dans le panier");
+    expect(afterLoading).toBeFalsy();
 })
