d7f8e2690367066f4e41d4f731e3ea11e4f342a4
Mehdi Mansour <elmehdi.mansour@gmail.com>
2022-04-15 15:59:18 +0200

    test different

--- ExecBack.sh
+++ ExecBack.sh
@@ -3,7 +3,7 @@ cd Back
 
 docker-compose build
 docker-compose up -d
-docker-compose exec -T app "composer install"
-docker-compose exec -T app "php bin/console doctrine:migration:migrate"
-docker-compose exec -T app "php bin/console doctrine:fixtures:load"
-docker-compose exec -T app "./vendor/bin/phpunit"
+docker-compose exec -T app composer install
+docker-compose exec -T app php bin/console doctrine:migration:migrate
+docker-compose exec -T app php bin/console doctrine:fixtures:load
+docker-compose exec -T app ./vendor/bin/phpunit
