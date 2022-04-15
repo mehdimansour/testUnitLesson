cff6b78687a42d1693bb0e0df757cb0e31e9fd91
Mehdi Mansour <elmehdi.mansour@gmail.com>
2022-04-15 16:05:57 +0200

    adding the test DB

--- ExecBack.sh
+++ ExecBack.sh
@@ -6,4 +6,7 @@ docker-compose up -d
 docker-compose exec -T app composer install
 docker-compose exec -T app php bin/console doctrine:migration:migrate
 docker-compose exec -T app php bin/console doctrine:fixtures:load
+docker-compose exec -T app php bin/console doctrine:database:drop --force --env=test
+docker-compose exec -T app php bin/console doctrine:database:create --env=test
+docker-compose exec -T app php bin/console doctrine:migrations:migrate -n --env=test
 docker-compose exec -T app ./vendor/bin/phpunit
