0e692d2d917736dc475f3924b4aad4112b99b0f7
Mehdi Mansour <elmehdi.mansour@gmail.com>
2022-04-15 15:45:12 +0200

    test

--- ExecBack.sh
+++ ExecBack.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd Back
+$VARIA = pwd
 docker-compose build
 docker-compose up -d
-docker-compose exec -T container pwd "composer install"
-docker-compose exec -T container pwd "php bin/console doctrine:migration:migrate"
-docker-compose exec -T container pwd "php bin/console doctrine:fixtures:load"
-docker-compose exec -T container pwd "./vendor/bin/phpunit"
+docker-compose exec -T app "composer install"
+docker-compose exec -T app "php bin/console doctrine:migration:migrate"
+docker-compose exec -T app "php bin/console doctrine:fixtures:load"
+docker-compose exec -T app "./vendor/bin/phpunit"
